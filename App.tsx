5749074e27db63dc5e5ccfa967db2427fdb996ea
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-12-18 14:36:34 +0500

    'commit'

--- App.tsx
+++ App.tsx
@@ -7,14 +7,18 @@ import TopTabBar from './src/navigation/TopTabBar'
 import { createStackNavigator } from '@react-navigation/stack'
 import ChatScreen from './src/screens/ChatScreen'
 import ContactScreen from './src/screens/ContactScreen'
-
+import AuthScreen from './src/screens/AuthScreen'
+ 
 const Stack = createStackNavigator();
 
 export default function App () {
   return (
     <NavigationContainer >
         <StatusBar backgroundColor={Colors.primaryColor} style='light' />
+        
         <Stack.Navigator >
+        <Stack.Screen name='AuthScreen' component={AuthScreen} options={{headerShown:false}} />
+
           <Stack.Screen name='HomeScreen' component={HomeScreen} options={{headerShown:false}} />
           <Stack.Screen name='ChatScreen' component={ChatScreen} options={{headerShown:false}}/>
           <Stack.Screen name='ContactScreen' component={ContactScreen} options={{headerShown:false}}/>
---
+++ src/screens/AuthScreen.tsx
@@ -0,0 +1,15 @@
+
+
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function AuthScreen() {
+  return (
+    <View>
+      <Text>AuthScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
